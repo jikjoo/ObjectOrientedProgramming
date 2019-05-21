068b14404caed684589d288a9a11742d0e285cd0
jikjoo <pillast777@naver.com>
2019-05-21 20:23:26 +0900

    vector

--- Project4-SparseMatrix/SparseMatrix.h
+++ Project4-SparseMatrix/SparseMatrix.h
@@ -1,48 +1,68 @@
 #ifndef _SPARSE_MATRIX_H_
 #define _SPARSE_MATRIX_H_
 
-class SparseMatrix 
-{
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class SparseMatrix {
 public:
-	// nr x nc sparse matrix is constructed. If they are not given, 1x1 matrix by default 
-	SparseMatrix(int nr = 1,int nc = 1) { }	
-	~SparseMatrix() { }
-	
-	// resize the matrix to nr x nc. 
+	// nr x nc sparse matrix is constructed. If they are not given, 1x1 matrix by default
+	SparseMatrix(int nr = 1, int nc = 1) {
+		nRow = nr;
+		nCol = nc;
+	}
+	~SparseMatrix() {
+		vals.clear();
+		rows.clear();
+		cols.clear();
+	}
+
+	// resize the matrix to nr x nc.
 	// if increased, the added parts are filled with zeros
-	void resize(int nr,int nc);
-	
-	int getNumRows();	// return number of rows
-	int getNumCols();	// return number of columns
-	int getNumOfNonZeros();	// return number of non-zero elements
-	
+	void resize(int nr, int nc);
+
+	int getNumRows() { return nRow; }; // return number of rows
+	int getNumCols() { return nCol; }; // return number of columns
+	int getNumOfNonZeros();			   // return number of non-zero elements
+
 	// file access
 	// read a sparse matrix from a text file
-	// the file format is "row col val" 
+	// the file format is "row col val"
 	bool readFromFile(string filename);
-	
+
 	// data access
-	void setValue(int row,int col,double val); // write val at (i,j)
-	double getValue(int row,int col); // read value of (i,j)
+	void setValue(int row, int col, double val); // write val at (i,j)
+	double getValue(int row, int col);			 // read value of (i,j)
 
 	// operations
-	SparseMatrix operator+(SparseMatrix& M); // matrix addition
-	SparseMatrix operator-(SparseMatrix& M); // matrix subtraction
-	SparseMatrix operator*(SparseMatrix& M); // matrix multiplication
-	bool operator==(SparseMatrix& M);	// true if all elements are equal
-	SparseMatrix operator-();				// inverting the sign of all elements
+	SparseMatrix operator+(SparseMatrix &M); // matrix addition
+	SparseMatrix operator-(SparseMatrix &M); // matrix subtraction
+	SparseMatrix operator*(SparseMatrix &M); // matrix multiplication
+	bool operator==(SparseMatrix &M);		 // true if all elements are equal
+	SparseMatrix operator-();				 // inverting the sign of all elements
 
 	// summary
-	double rowSum(int row); // calculate the sum of elements in a "row"-th row
-	double colSum(int col); // calculate the sum of elements in a "col"-th column
-	double sum(); // calculate the sum of all elements
+	double rowSum(int row);			   // calculate the sum of elements in a "row"-th row
+	double colSum(int col);			   // calculate the sum of elements in a "col"-th column
+	double sum();					   // calculate the sum of all elements
 	bool isAllAbsLessThan(double val); // true if the absolute value of all elements are less than val
-
 
 private:
 	// declare whatever you want
+	vector<double> vals;
+	vector<int> rows, cols;
+	int nRow, nCol;
+	//map<int , double> vals;
 	
 };
 
-
+void SparseMatrix::setValue(int row, int col, double val) {
+	int idx = row * nCol + col;
+	//vals.insert(pair<int, double>(idx, val));
+}
+double SparseMatrix::getValue(int row, int col){
+	vector<int>::iterator iter;
+}
 #endif
